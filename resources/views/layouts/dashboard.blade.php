1fa1732d1ec91035aa2d102389cb0d25c03b4e59
sherwin <sherwinm@gmail.com>
2020-04-22 17:00:04 -0700

    added logo

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -11,7 +11,9 @@
 </head>
 <body>
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">{{ env('APP_NAME') }}</a>
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
+            <img src="/images/done_white.jpg" alt="Done logo" class="img-fluid" style="max-height:50px;">
+        </a>
 
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
