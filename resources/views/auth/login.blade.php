f533cb0fc58388766650792b17cdb42c264fe628
sherwin <sherwinm@gmail.com>
2020-04-22 17:00:18 -0700

    added logo and use form open() and close()

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.auth')
 
 @section('content')
-        <form class="form-signin">
+        {{ Form::open(['method' => 'POST', 'route' => 'login', 'class' => 'form-signin']) }}
+            <img src="/images/done_transparent.png" alt="Done logo" class="img-fluid">
             <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
             {{ Form::label('inputEmail', 'Email Address', ['class' => 'sr-only']) }}
             {{ Form::email('email', '', ['class' => 'form-control', 'placeholder' => 'Email address', 'required', 'autofocus', 'id' => 'inputEmail']) }}
@@ -17,5 +18,5 @@
 
             <a href="" class="btn btn-lg btn-block btn-outline-secondary mt-1">Password help</a>
             <p class="mt-5 mb-3 text-muted">{{ env('APP_NAME') }}</p>
-        </form>
+        {{ Form::close() }}
 @endsection
