6c58f6d77adc6e38753a0c34acc0fa2d3802ccd7
sherwin <sherwinm@gmail.com>
2020-04-22 21:10:28 -0700

    added crud methods and has many projects

--- app/ProjectStatus.php
+++ app/ProjectStatus.php
@@ -7,4 +7,61 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectStatus extends Model
 {
     protected $table = 'project_statuses';
+
+    /**
+     * Has many projects.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    public static function storeRecord($request)
+    {
+        $project_status = new ProjectStatus;
+
+        $project_status->name       = $request->name;
+
+        if ($project_status->save())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Update record.
+     * @param $request
+     * @return bool
+     */
+    public static function updateRecord($request)
+    {
+        $project_status = ProjectStatus::find($request->id);
+
+        $project_status->name       = $request->name;
+
+        if ($project_status->save())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Delete record.
+     * @param $request
+     * @return bool
+     */
+    public static function deleteRecord($request)
+    {
+        if (ProjectStatus::find($request->id)->delete())
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
