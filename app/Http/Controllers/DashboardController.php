c86a632027dd8ed649b3ee02237c87e68befc05d
sherwin <sherwinm@gmail.com>
2020-04-24 20:24:15 -0700

    filled out page title and added new objects

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Company;
+use App\MyCompany;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -11,12 +14,19 @@ class DashboardController extends Controller
         $this->middleware('auth');
     }
 
+    /**
+     * Display index page.
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $data = [
-            'page_title'        => '',
+            'page_title'        => 'My Dashboard',
             'navi_group'        => '',
-            'navi_submenu'      => ''
+            'navi_submenu'      => '',
+            'company_users'     => MyCompany::getUsers(Auth::User()->company_id)
+                                    ->get(),
+            'company'           => Company::find(Auth::User()->company_id)
         ];
 
         return view('dashboard', $data);
