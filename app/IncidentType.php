15b4c23e59ade942a40db2b2a9df7177b68f529f
sherwin <sherwinm@gmail.com>
2020-04-24 21:02:20 -0700

    added crud methods

--- app/IncidentType.php
+++ app/IncidentType.php
@@ -16,4 +16,57 @@ class IncidentType extends Model
     {
         return $this->hasMany(Incident::class);
     }
+
+    /**
+     * Store record.
+     * @param $request
+     * @return bool
+     */
+    public static function storeRecord($request)
+    {
+        $incident_type = new IncidentType;
+
+        $incident_type->name        = $request->name;
+
+        if ($incident_type->save())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Update record.
+     * @param $request
+     * @return bool
+     */
+    public static function updateRecord($request)
+    {
+        $incident_type = IncidentType::find($request->id);
+
+        $incident_type->name        = $request->name;
+
+        if ($incident_type->save())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Delete record.
+     * @param $request
+     * @return bool
+     */
+    public static function deleteRecord($request)
+    {
+        if (IncidentType::find($request->id)->delete)
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
