f31abe3703892a8a2ed73d61794414ca74a01276
sherwin m <sherwinm@gmail.com>
2020-06-30 10:13:58 -0700

    make button smaller and use icon

--- resources/views/incident_types/index.blade.php
+++ resources/views/incident_types/index.blade.php
@@ -1,28 +1,37 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    @if ($incident_types)
-        <table class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>&nbsp;</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($incident_types as $incident_type)
-                    <tr>
-                        <td>{{ $incident_type->name }}</td>
-                        <td>{{ $incident_type->description }}</td>
-                        <td><a href="{{ route('incidentTypes.edit', ['incidentType' => $incident_type->id]) }}" class="btn btn-outline-secondary">edit</a></td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @else
-        <div class="alert alert-info">
-            No incident type found.
+    <section class="card">
+        <div class="card-body">
+            <h2>{{ $page_title }}</h2>
+            @if ($incident_types)
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($incident_types as $incident_type)
+                            <tr>
+                                <td>{{ $incident_type->name }}</td>
+                                <td>{{ $incident_type->description }}</td>
+                                <td>
+                                    <a href="{{ route('incidentTypes.edit', ['incidentType' => $incident_type->id]) }}" class="btn btn-sm btn-outline-secondary">
+                                        <i class="fas fa-wrench"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <div class="alert alert-info">
+                    No incident type found.
+                </div>
+            @endif
         </div>
-    @endif
+    </section>
 @endsection
