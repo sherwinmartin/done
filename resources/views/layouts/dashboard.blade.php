4ba3b1bc2ea398dc1eae221bc9837d537e68fc41
sherwin m <sherwinm@gmail.com>
2020-06-30 10:17:49 -0700

    add padding between nav and content

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -31,9 +31,9 @@
         </ul>
     </nav>
 
-    <div class="container-fluid mt-5">
+    <div class="container-fluid mt-5 pt-5">
         <div class="row">
-            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+            <nav class="col-md-2 d-none d-md-block sidebar">
                 <div class="sidebar-sticky">
                     <ul class="nav flex-column">
                         <li class="nav-item">
