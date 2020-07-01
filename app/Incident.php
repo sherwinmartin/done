2ea713ab229573bc9edb0e2370fa03d6933a44ef
sherwin m <sherwinm@gmail.com>
2020-06-30 20:02:20 -0700

    added search()

--- app/Incident.php
+++ app/Incident.php
@@ -25,4 +25,27 @@ class Incident extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * Search record.
+     * @param $request
+     * @return mixed
+     */
+    public static function search($request)
+    {
+        return Incident::where(function ($query) use ($request)
+        {
+            if ($request->incident_type_id)
+            {
+                $query->where('incident_type_id', $request->incident_type_id);
+            }
+
+            if ($request->user_id)
+            {
+                $query->where('user_id', $request->user_id);
+            }
+        })->with('incidentType')
+            ->with('user');
+
+    }
 }
