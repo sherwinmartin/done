30ff925b11a97f7bd14dedf292146a05be54a2e8
sherwin m <sherwinm@gmail.com>
2020-06-30 18:10:19 -0700

    finished coding crud functionality

--- app/Http/Controllers/IncidentTypeController.php
+++ app/Http/Controllers/IncidentTypeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\IncidentTypeRequest;
 use App\IncidentType;
 use Illuminate\Http\Request;
 
@@ -24,6 +25,37 @@ class IncidentTypeController extends Controller
     }
 
     /**
+     * Display create form.
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        $data = [
+            'page_title'            => 'Create Incident Type',
+            'navi_group'            => 'incidentTypes',
+            'navi_submenu'          => 'incidentTypes.create',
+            'incident_type'         => new IncidentType
+        ];
+
+        return view('incident_types.create',$data);
+    }
+
+    /**
+     * Store record.
+     * @param IncidentTypeRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(IncidentTypeRequest $request)
+    {
+        if (IncidentType::storeRecord($request))
+        {
+            return back()->with('success', 'Incident type created.');
+        }
+
+        return back()->withInput()->with('error', 'Incident type failed to be created.');
+    }
+
+    /**
      * Edit record.
      * @param IncidentType $incidentType
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
@@ -41,5 +73,35 @@ class IncidentTypeController extends Controller
         ];
 
         return view('incident_types.edit', $data);
+    }
+
+    /**
+     * Update record.
+     * @param IncidentTypeRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(IncidentTypeRequest $request)
+    {
+        if (IncidentType::updateRecord($request))
+        {
+            return back()->with('success', 'Incident type updated.');
+        }
+
+        return back()->withInput()->with('error', 'Incident type failed to be updated.');
+    }
+
+    /**
+     * Delete record.
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Request $request)
+    {
+        if (IncidentType::deleteRecord($request))
+        {
+            return redirect()->route('incidentTypes.index')->with('success', 'Incident type deleted.');
+        }
+
+        return back()->with('error', 'Incident type failed to be deleted.');
     }
 }
